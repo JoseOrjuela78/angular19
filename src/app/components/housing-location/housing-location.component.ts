e2ffd7372734616ca6cbe3a61bdb0d3451716797
JoseOrjuela78 <alfredo.orjuela@gmail.com>
2025-05-13 12:24:41 -0500

    Angular 19

---
+++ src/app/components/housing-location/housing-location.component.ts
@@ -0,0 +1,18 @@
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { HousingLocation } from '../housing-location';
+import { RouterModule } from '@angular/router';
+
+
+@Component({
+  selector: 'app-housing-location',
+  imports: [RouterModule],
+  templateUrl: './housing-location.component.html',
+  styleUrl: './housing-location.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HousingLocationComponent {
+
+  @Input() housingLocation!: HousingLocation;
+
+
+}
